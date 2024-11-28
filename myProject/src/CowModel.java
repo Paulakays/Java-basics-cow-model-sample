c901c6913cd0fc839cbb2d9d5654b7d631b47b79
Wanekeya <anythingpaula@gmail.com>
2024-11-28 14:19:54 +0100

    Basic java

---
+++ myProject/src/CowModel.java
@@ -0,0 +1,9 @@
+public class CowModel {
+        public static void main(String[] args) {
+            System.out.println("\t\t\\_ _/ \\_ _/");
+            System.out.println("\t\t(\" \") (\" \")");
+            System.out.println("   .-----\\ /   \\ /-----.");
+            System.out.println("  /|______|\t\t|______|\\");
+            System.out.println(" \" | \\  ||\t\t || / |\t\"");
+        }
+    }
